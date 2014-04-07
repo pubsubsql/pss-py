372c4672f73eb209962abaf4af5f50fcc99ead14
Mike (Mykhailo) Oksenenko <mmx.mmx@gmail.com>
2014-04-06 20:41:09 -0400

    add testDeleteOneRow

--- src/pubsubsql/testclient.py
+++ src/pubsubsql/testclient.py
@@ -181,29 +181,41 @@ class TestClient(unittest.TestCase):
         self.assertFalse(client.nextRow())
         client.disconnect()
 
-        def testUpdateOneRow(self):
-            tableName = self.__generateTableName()
-            self.__insertRow(tableName)
-            #
-            client = Client()
-            client.connect(self.__ADDRESS())
-            command = "update {} set col1 = newvalue".format(tableName)
-            client.execute(command)
-            self.assertEqual("update", client.getAction())
-            self.assertEqual(1, client.getRowCount())
-            client.disconnect()
-    
-        def testUpdateManyRow(self):
-            tableName = self.__generateTableName()
-            self.__insertRows(tableName)
-            #
-            client = Client()
-            client.connect(self.__ADDRESS())
-            command = "update {} set col1 = newvalue".format(tableName)
-            client.execute(command)
-            self.assertEqual("update", client.getAction())
-            self.assertEqual(self.__ROWS(), client.getRowCount())
-            client.disconnect()
+    def testUpdateOneRow(self):
+        tableName = self.__generateTableName()
+        self.__insertRow(tableName)
+        #
+        client = Client()
+        client.connect(self.__ADDRESS())
+        command = "update {} set col1 = newvalue".format(tableName)
+        client.execute(command)
+        self.assertEqual("update", client.getAction())
+        self.assertEqual(1, client.getRowCount())
+        client.disconnect()
+
+    def testUpdateManyRow(self):
+        tableName = self.__generateTableName()
+        self.__insertRows(tableName)
+        #
+        client = Client()
+        client.connect(self.__ADDRESS())
+        command = "update {} set col1 = newvalue".format(tableName)
+        client.execute(command)
+        self.assertEqual("update", client.getAction())
+        self.assertEqual(self.__ROWS(), client.getRowCount())
+        client.disconnect()
+
+    def testDeleteOneRow(self):
+        tableName = self.__generateTableName()
+        self.__insertRow(tableName)
+        #
+        client = Client()
+        client.connect(self.__ADDRESS())
+        command = "delete from {}".format(tableName)
+        client.execute(command)
+        self.assertEqual("delete", client.getAction())
+        self.assertEqual(1, client.getRowCount())
+        client.disconnect()
     
 if __name__ == "__main__":
     unittest.main()
